1014cca989a39f5d922e747777dea17ac18f8013
Derek Groen <djgroennl@gmail.com>
2020-04-23 12:12:56 +0100

    Added test cases and amended read_age_csv to work with test problem.

--- readers/read_age_csv.py
+++ readers/read_age_csv.py
@@ -5,6 +5,8 @@ import sys
 def read_age_csv(csv_name, header_name=""):
   df = pd.read_csv(csv_name)
   df.columns = map(str.lower, df.columns)
+  if header_name not in df.columns:
+    header_name = "United Kingdom"
   ages = df[header_name.lower()].to_numpy()
   return ages / np.sum(ages)
 
