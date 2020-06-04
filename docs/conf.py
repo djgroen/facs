606ec6249d8b3b9236a65cb896895647093efb64
Diana Suleimenova <diana.suleimenova@brunel.ac.uk>
2020-06-05 00:06:00 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,10 +47,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 master_doc = 'index'
+
+html_theme = 'msmb_theme'
+import msmb_theme
+html_theme_path = [msmb_theme.get_html_theme_path()]
+
