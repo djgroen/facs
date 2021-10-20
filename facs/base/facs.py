05e5a0a146d03a51f2bf1c783f0c06d4e3d0f41f
Derek Groen <djgroennl@gmail.com>
2021-10-20 13:36:01 +0100

    Added some explanation and fixed wrong values in track and trace efficiency.

--- facs/base/facs.py
+++ facs/base/facs.py
@@ -726,9 +726,9 @@ class Ecosystem:
     self.loc_groups = {}
     self.needsfile = needsfile
 
-    self.airflow_indoors_small = 10
-    self.airflow_indoors_large = 40.0
-    self.airflow_outdoors = 200.0
+    self.airflow_indoors_small = 9.0
+    self.airflow_indoors_large = 36.0
+    self.airflow_outdoors = 180.0
 
     self.external_travel_multiplier = 1.0 # Can be adjusted to introduce peaks in external travel, e.g. during holiday returns or major events (Euros).
     self.external_infection_ratio = 0.5 # May be changed at runtime. Base assumption is that there are 300% extra external visitors, and that 1% of them have COVID. Only applies to transport for now.
