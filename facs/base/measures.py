c92760e1b8169c8e5a35bcbdb7ee20b1504e281f
Derek Groen <djgroennl@gmail.com>
2022-05-06 11:26:47 +0100

    4.

--- facs/base/measures.py
+++ facs/base/measures.py
@@ -126,15 +126,15 @@ def read_lockdown_yml(e, ymlfile="covid_data/measures.yml"):
 
 
 def update_hospital_protection_factor_uk(e, t):
-  e.hospital_protection_factor = 0.5
+  e.hospital_protection_factor = 0.7
   if t == 20:
-    e.hospital_protection_factor = 0.4
+    e.hospital_protection_factor = 0.6
   if t == 30: # start of testing ramp up in early april.
-    e.hospital_protection_factor = 0.35
+    e.hospital_protection_factor = 0.5
   if t == 40:
-    e.hospital_protection_factor = 0.3
+    e.hospital_protection_factor = 0.4
   if t == 50:
-    e.hospital_protection_factor = 0.25
+    e.hospital_protection_factor = 0.3
   if t == 60: # testing ramped up considerably by the end of April.
     e.hospital_protection_factor = 0.2
   if t == 80:
