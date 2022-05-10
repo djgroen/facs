74d4e5ca5be9ae2c626e68be77f893b9d3ce9eaf
Derek Groen <djgroennl@gmail.com>
2022-05-10 11:00:01 +0100

    Reverting run.py and facs.py, and moving grid run.py to run_grid.py

--- run_grid.py
+++ run_grid.py
@@ -17,32 +17,25 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
 
-    # Instantiate the parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--location', action="store", default="brent", help="Name of location to load.")
-    parser.add_argument('-m','--measures_yml', action="store", default="measures_uk", help="Input YML file containing interventions.")
-    parser.add_argument('-d','--disease_yml', action="store", default="disease_covid19", help="Input YML file containing disease specification.")
-    parser.add_argument('-v','--vaccinations_yml', action="store", default="vaccinations", help="Input YML file containing vaccine strategy specification.")
-    parser.add_argument('--output_dir', action="store", default=".", help="directory to write output to.")
-    parser.add_argument('--data_dir', action="store", default="covid_data", help="subdirectory containing simulation input data.")
-    parser.add_argument('-s','--starting_infections', action="store", default="500", help="Starting # of infections. Values below 1.0 are interpreted as a ratio of population.")
-    parser.add_argument('--start_date', action="store", default="1/3/2020", help="Start date, format = %d/%m/%Y")
-    parser.add_argument('-q', '--quicktest', action="store_true", help="set house_ratio to 100 to do quicker (but less accurate) runs for populous regions.")
-    parser.add_argument('-g', '--generic_outfile', action="store_true", help="Write main output to out.csv instead of a scenario-specific named file.")
-    parser.add_argument('--dbg', action="store_true", help="Write additional outputs to help debugging")
-    parser.add_argument('-t', '--simulation_period', action="store",type=int, default='-1', help="Simulation duration [days].")
-    args = parser.parse_args()
-    print(args)
+    # Instantiate the parameters
+    region = sys.argv[1]
+    measures_yml = sys.argv[2]
+    disease_yml = sys.argv[3]
+    vaccinations_yml = sys.argv[4]
+    output_dir = sys.argv[5]
+    data_dir = sys.argv[6]
+    starting_infections = sys.argv[7]
+    start_date = sys.argv[8]
+    quicktest = int(sys.argv[9])
+    generic_outfile = int(sys.argv[10])
+    dbg = int(sys.argv[11])
+    simulation_period = int(sys.argv[12])
+
+    facs.region = region
 
     house_ratio = 2
-    if args.quicktest:
+    if quicktest == 1:
       house_ratio = 100
-    location = args.location
-    measures_yml = args.measures_yml
-    disease_yml = args.disease_yml
-    vaccinations_yml = args.vaccinations_yml
-    output_dir = args.output_dir
-    data_dir = args.data_dir
 
     # if simsetting.csv exists -> overwrite the simulation setting parameters
     if path.isfile('simsetting.csv'):
@@ -60,18 +53,18 @@ if __name__ == "__main__":
         makedirs(output_dir)
 
     outfile = "{}/{}-{}.csv".format(output_dir,
-                                       location,
+                                       region,
                                        measures_yml)
-    if args.generic_outfile:
+    if generic_outfile == 1:
       outfile = "{}/out.csv".format(output_dir)
 
     end_time = 1100
     
-    if args.simulation_period > 0:
-      end_time = args.simulation_period
+    if simulation_period > 0:
+      end_time = simulation_period
     
     print("Running basic Covid-19 simulation kernel.")
-    print("scenario = %s" % (location))
+    print("scenario = %s" % (region))
     print("measures input yml = %s" % (measures_yml))
     print("disease input yml = %s" % (disease_yml))
     print("vaccinations input yml = %s" % (vaccinations_yml))
@@ -80,16 +73,16 @@ if __name__ == "__main__":
     print("outfile  = %s" % (outfile))
     print("data_dir  = %s" % (data_dir))
 
-    e = facs.Ecosystem(end_time)
+    e = facs.Ecosystem(facs.region, end_time)
 
-    e.ages = read_age_csv.read_age_csv("{}/age-distr.csv".format(data_dir), location)
+    e.ages = read_age_csv.read_age_csv("{}/age-distr.csv".format(data_dir), region)
 
     print("age distribution in system:", e.ages, file=sys.stderr)
 
     e.disease = read_disease_yml.read_disease_yml(
         "{}/{}.yml".format(data_dir, disease_yml))
 
-    building_file = "{}/{}_buildings.csv".format(data_dir, location)
+    building_file = "{}/{}_buildings.csv".format(data_dir, region)
     read_building_csv.read_building_csv(e,
                                         building_file,
                                         "{}/building_types_map.yml".format(data_dir),
@@ -100,28 +93,28 @@ if __name__ == "__main__":
     # household size: average size of each household, specified separately here.
     # work participation rate: fraction of population in workforce, irrespective of age
 
-    #print("{}/{}_cases.csv".format(data_dir, location))
+    #print("{}/{}_cases.csv".format(data_dir, region))
     # Can only be done after houses are in.
     #read_cases_csv.read_cases_csv(e,
-    #                              "{}/{}_cases.csv".format(data_dir, location),
+    #                              "{}/{}_cases.csv".format(data_dir, region),
     #                              start_date=args.start_date,
     #                              date_format="%m/%d/%Y")
 
     e.print_status(outfile, silent=True) # silent print to initialise log data structures.
 
     starting_num_infections = 500
-    if args.starting_infections:
-        if int(args.starting_infections[0]) == 0:
-            starting_num_infections = int(e.num_agents*float(args.starting_infections))
+    if starting_infections:
+        if int(starting_infections[0]) == 0:
+            starting_num_infections = int(e.num_agents*float(starting_infections))
         else:
-            starting_num_infections = int(args.starting_infections)
-    elif location == "test":
+            starting_num_infections = int(starting_infections)
+    elif region == "test":
         starting_num_infections = 10
 
     print("THIS SIMULATIONS HAS {} AGENTS. Starting with {} infections.".format(e.num_agents, starting_num_infections))
 
     e.time = -20
-    e.date = datetime.strptime(args.start_date, "%d/%m/%Y")
+    e.date = datetime.strptime(start_date, "%d/%m/%Y")
     e.date = e.date - timedelta(days=20)
     e.print_header(outfile)
     for i in range(0, 20):
@@ -138,7 +131,7 @@ if __name__ == "__main__":
         e.evolve(reduce_stochasticity=False)
 
         print(e.time)
-        if args.dbg:
+        if dbg == 1:
             e.debug_mode = True
             e.print_status(outfile)
         else:
