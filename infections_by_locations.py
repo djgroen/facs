7f769a7994eeafc097ec327b17e06341ad45fe51
Arindam Saha <arindamsaha1507@gmail.com>
2022-05-20 07:31:03 +0100

    Reduced the amount of text output and cleaned up the code

--- infections_by_locations.py
+++ infections_by_locations.py
@@ -2,56 +2,56 @@ import pandas as pd
 import plotly.express as px
 import datetime as dt
 
-df = pd.read_csv('covid_out_infections_0.csv')
+def location_timeline():
+    df = pd.read_csv('covid_out_infections_0.csv')
 
-start_date = dt.datetime.strptime('2020 September 1', '%Y %B %d').date()
-df['date'] = [start_date + dt.timedelta(days=x) for x in df['#time']]
+    start_date = dt.datetime.strptime('2020 September 1', '%Y %B %d').date()
+    df['date'] = [start_date + dt.timedelta(days=x) for x in df['#time']]
 
-df = df[['date', 'location_type']]
-df['number'] = 0
+    df = df[['date', 'location_type']]
+    df['number'] = 0
 
-print(df)
+    tab = df.groupby(['date', 'location_type'])['number'].count()
+    tab = pd.DataFrame(tab.to_frame()).reset_index()
 
-tab = df.groupby(['date', 'location_type'])['number'].count()
-
-tab = pd.DataFrame(tab.to_frame()).reset_index()
-
-print(tab)
-
-dd = pd.DataFrame()
-end_date = max(df['date'])
-num_days = (end_date - start_date).days
-dd['date'] = [start_date + dt.timedelta(days=x) for x in range(num_days+1)]
-tt = list(set(tab['location_type']))
-tt.sort()
-print(tt)
-for t in tt:
-    dd[t] = 0
-
-
-for i in range(len(dd)):
-    curr_date = dd.iloc[i]['date']
-
-    temp = tab[tab['date']==curr_date]
-
+    dd = pd.DataFrame()
+    end_date = max(df['date'])
+    num_days = (end_date - start_date).days
+    dd['date'] = [start_date + dt.timedelta(days=x) for x in range(num_days+1)]
+    tt = list(set(tab['location_type']))
+    tt.sort()
     for t in tt:
-        data = list(temp[temp['location_type']==t]['number'])
-        if len(data) > 0:
-            dd[t].iloc[i] = data[0]
+        dd[t] = 0
 
-print(dd)
+    for i in range(len(dd)):
+        curr_date = dd.iloc[i]['date']
 
-dd = dd.set_index('date')
-fig = px.line(dd)
-fig.show()
+        temp = tab[tab['date']==curr_date]
 
-rd = dt.datetime.strptime('2020 September 30', '%Y %B %d').date()
+        for t in tt:
+            data = list(temp[temp['location_type']==t]['number'])
+            if len(data) > 0:
+                dd[t].iloc[i] = data[0]
 
-df = pd.read_csv('covid_out_infections_0.csv')
-df['date'] = [start_date + dt.timedelta(days=x) for x in df['#time']]
-mp = df[df['date'] == rd]
+    dd = dd.set_index('date')
+    fig = px.line(dd)
+    fig.show()
 
-fig = px.scatter_mapbox(df, lat='y', lon='x', color='location_type', zoom=10.3)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+def map_plotter(date='2020 September 30'):
+
+    start_date = dt.datetime.strptime('2020 September 1', '%Y %B %d').date()
+
+    rd = dt.datetime.strptime(date, '%Y %B %d').date()
+
+    df = pd.read_csv('covid_out_infections_0.csv')
+    df['date'] = [start_date + dt.timedelta(days=x) for x in df['#time']]
+    mp = df[df['date'] == rd]
+
+    fig = px.scatter_mapbox(df, lat='y', lon='x', color='location_type', zoom=10.3)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
+if __name__ == '__main__':
+    location_timeline()
+    map_plotter()
