edd3b3ceaa1cd89b50364de0381694c679001e81
Derek Groen <djgroennl@gmail.com>
2022-06-15 17:37:32 +0100

    Tiny alpha adjustment.

--- facs/base/measures.py
+++ facs/base/measures.py
@@ -137,7 +137,7 @@ def update_hospital_protection_factor_uk(e, t):
 
 def calculate_mutating_infection_rate(fraction, source=0.07, dest=0.1):
   # Original infection rate is 0.07 (COVID-19 disease.yml)
-  # destination infection rate is "up to 70% higher", so we set it to 0.07*1.5=0.105.
+  # destination infection rate is "up to 70% higher", so we set it to 0.07*1.6=0.112.
 
   if fraction > 1.0:
     print("Error: fraction > 1.0", file=sys.stderr)
@@ -153,7 +153,7 @@ def enact_measures_and_evolutions(e, t, measures_yml, vaccinations_yml):
   if t > 235 and t < 316:
     a = e.disease.infection_rate
     fraction = (t - 235) * 0.0125
-    e.disease.infection_rate = calculate_mutating_infection_rate(fraction, 0.07, 0.11) # https://cmmid.github.io/topics/covid19/uk-novel-variant.html
+    e.disease.infection_rate = calculate_mutating_infection_rate(fraction, 0.07, 0.112) # https://cmmid.github.io/topics/covid19/uk-novel-variant.html
     print("infection rate adjusted from ",a," to ", e.disease.infection_rate, file=sys.stderr)
 
 
@@ -161,7 +161,7 @@ def enact_measures_and_evolutions(e, t, measures_yml, vaccinations_yml):
   # Prevalence increases linearly from Apr 21 (1%) to June 10th (100%)
   if t > 416 and t < 467:
     fraction = (t - 416) * 0.02
-    e.disease.infection_rate = calculate_mutating_infection_rate(fraction, 0.11, 0.165) # https://www.ecdc.europa.eu/en/publications-data/threat-assessment-emergence-and-impact-sars-cov-2-delta-variant
+    e.disease.infection_rate = calculate_mutating_infection_rate(fraction, 0.112, 0.165) # https://www.ecdc.europa.eu/en/publications-data/threat-assessment-emergence-and-impact-sars-cov-2-delta-variant
 
     # our estimate is 50% here, as Delta gains full dominance in this period.
     # https://www.gov.uk/government/news/confirmed-cases-of-covid-19-variants-identified-in-uk#:~:text=The%20Delta%20variant%20currently%20accounts,of%20cases%20across%20the%20UK.&text=In%20total%2C%203%2C692%20people%20have,the%20Delta%20and%20Beta%20variants.l
