1b2fd884991a29e5984602de7d44e57852c686b3
Arindam Saha <arindamsaha1507@gmail.com>
2022-08-07 11:51:28 +0100

    Removed redundant lines

--- dump_nearest_locations.py
+++ dump_nearest_locations.py
@@ -20,98 +20,29 @@ if __name__ == "__main__":
     # Instantiate the parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--location', action="store", default="brent")
-    parser.add_argument('--transition_scenario', action="store", default="extend-lockdown")
-    parser.add_argument('--transition_mode', action="store",
-                        type=int, default='1')
     parser.add_argument('--ci_multiplier', action="store",
                         type=float, default='0.625', help="Multiplier set for Case Isolation which represents the ratio of out-of-house interactions for Covid patients relative to the default interaction rate. Default value comes from Imp Report 9.")
     parser.add_argument('--output_dir', action="store", default=".")
     parser.add_argument('--data_dir', action="store", default="covid_data")
-    parser.add_argument('-s','--starting_infections', action="store", default="500")
-    parser.add_argument('--start_date', action="store", default="1/3/2020")
-    parser.add_argument('-q', '--quicktest', action="store_true", help="set house_ratio to 100 to do quicker (but less accurate) runs for populous regions.")
-    parser.add_argument('-g', '--generic_outfile', action="store_true", help="Write main output to out.csv instead of a scenario-specific named file.")
     parser.add_argument('--dbg', action="store_true", help="Write additional outputs to help debugging")
-    parser.add_argument('--simulation_period', action="store",type=int, default='-1')
     args = parser.parse_args()
     print(args)
 
     house_ratio = 2
-    if args.quicktest:
-      house_ratio = 100
     location = args.location
     ci_multiplier = float(args.ci_multiplier)
-    transition_scenario = args.transition_scenario.lower()
-    transition_mode = args.transition_mode
     output_dir = args.output_dir
     data_dir = args.data_dir
 
-    # if simsetting.csv exists -> overwrite the simulation setting parameters
-    if path.isfile('simsetting.csv'):
-        with open('simsetting.csv', newline='') as csvfile:
-            values = csv.reader(csvfile)
-            for row in values:
-                if len(row) > 0:  # skip empty lines in csv
-                    if row[0][0] == "#":
-                        pass
-                    elif row[0].lower() == "transition_scenario":
-                        transition_scenario = str(row[1]).lower()
-                    elif row[0].lower() == "transition_mode":
-                        transition_mode = int(row[1])
-
     transition_day = -1
-    # if transition_mode == 1:
-    #     transition_day = 77  # 15th of April
-    # if transition_mode == 2:
-    #     transition_day = 93  # 31st of May
-    # if transition_mode == 3:
-    #     transition_day = 108  # 15th of June
-    # if transition_mode == 4:
-    #     transition_day = 123  # 30th of June
-    # if transition_mode > 10:
-    #     transition_day = transition_mode
 
-    # check the transition scenario argument
-    AcceptableTransitionScenario = ['no-measures', 'extend-lockdown',
-                                    'open-all', 'open-schools', 'open-shopping',
-                                    'open-leisure', 'work50', 'work75',
-                                    'work100', 'dynamic-lockdown', 'periodic-lockdown','uk-forecast']
-
-    if transition_scenario not in AcceptableTransitionScenario:
-        print("\nError !\n\tThe input transition scenario, %s , is not VALID" %
-              (transition_scenario))
-        print("\tThe acceptable inputs are : [%s]" %
-              (",".join(AcceptableTransitionScenario)))
-        sys.exit()
-
-    # check if output_dir is exists
     if not path.exists(output_dir):
         makedirs(output_dir)
 
-    outfile = "{}/{}-{}-{}.csv".format(output_dir,
-                                       location,
-                                       transition_scenario,
-                                       transition_day)
-    if args.generic_outfile:
-      outfile = "{}/out.csv".format(output_dir)
-
     end_time = 1100
-    if transition_scenario in ["extend-lockdown","dynamic-lockdown","periodic-lockdown","uk-forecast"]:
-      end_time = 1100
-    
-    if args.simulation_period > 0:
-      end_time = args.simulation_period
     
     print("Running basic Covid-19 simulation kernel.")
     print("scenario = %s" % (location))
-    print("transition_scenario = %s" % (transition_scenario))
-    print("transition_mode = %d" % (transition_mode))
-    print("transition_day = %d" % (transition_day))
-    print("end_time = %d" % (end_time))
-    print("output_dir  = %s" % (output_dir))
-    print("outfile  = %s" % (outfile))
-    print("data_dir  = %s" % (data_dir))
-
 
     e = facs.Ecosystem(end_time)
 
@@ -121,7 +52,7 @@ if __name__ == "__main__":
     print("age distribution in system:", e.ages, file=sys.stderr)
 
     e.disease = read_disease_yml.read_disease_yml(
-        "{}/disease.yml".format(data_dir))
+        "{}/disease_covid19.yml".format(data_dir))
 
     building_file = "{}/{}_buildings.csv".format(data_dir, location)
     read_building_csv.read_building_csv(e,
