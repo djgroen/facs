dae4247c965679d85837e01e121377c007ca3720
Arindam Saha <arindamsaha1507@gmail.com>
2023-02-07 15:43:29 +0000

    Debugged commands and removed unnecessary commands

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 -r requirements.txt
+pip3 install -r requirements.txt
 mkdir -p $HOME/opt/openmpi
 cd $HOME/Downloads
 wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.2.tar.gz
@@ -11,6 +11,3 @@ make all && make install
 export CC=$HOME/opt/openmpi/bin/mpicc
 export PATH=$PATH:$HOME/opt/openmpi/bin
 pip3 install mpi4py
-mkdir $HOME/workspace & cd $HOME/workspace
-git clone https://github.com/djgroen/facs
-cd facs/ & mpirun –n 8 python ./run.py 
